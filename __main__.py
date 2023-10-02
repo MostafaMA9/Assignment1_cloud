6229097d032ee813a3333b4612b2c3966103eac2
MaelCossec <mael.cossec@imt-atlantique.net>
2023-10-02 18:13:52 -0400

    main instance definitions

--- __main__.py
+++ __main__.py
@@ -1,7 +1,15 @@
-import SecurityGroup
-import ELB
-import instances
-import KeyPair
+from SecurityGroup import *
+from ELB import *
+from instances import *
+from KeyPair import *
 
 def main():
-    print("Hello World!")
+    create_vpc(1,'')
+    create_subnet(1,'',1)
+    create_subnet(2,'',1)
+
+    create_security_group('my-security-group', 'My security group', 'vpc-0b0b0b0b0b0b0b0b0')
+    create_key_pair('my-key-pair')
+
+    create_instances('ami-0b898040803850657', 't2.large', 4, 1, 'my-security-group', 'my-key-pair')
+    create_instances('ami-0b898040803850657', 'm4.large', 5, 2,'my-security-group', 'my-key-pair')
--- instances.py
+++ instances.py
@@ -2,10 +2,35 @@
 import boto3
 from botocore.exceptions import ClientError
 
-def create_instances(number, image_id, instance_type, security_group_id, key_name):
+def create_instances(image_id, instance_type, number, subnet_id, security_group_id, key_name):
     ec2 = boto3.client('ec2')
     try:
-        response = ec2.run_instances(ImageId=image_id, InstanceType=instance_type, MinCount=1, MaxCount=number, SecurityGroupIds=[security_group_id], KeyName=key_name)
+        response = ec2.run_instances(
+            ImageId=image_id, 
+            InstanceType=instance_type, 
+            Count=number,
+            SubnetId=subnet_id,
+            SecurityGroupIds=[security_group_id], 
+            KeyName=key_name)
+        print(response)
+    except ClientError as e:
+        print(e)
+
+def create_vpc(id, cidr_block):
+    ec2 = boto3.client('ec2')
+    try:
+        response = ec2.create_vpc(Id=id,CidrBlock=cidr_block)
+        print(response)
+    except ClientError as e:
+        print(e)
+
+def create_subnet(id, cidr_block, vpc_id):
+    ec2 = boto3.client('ec2')
+    try:
+        response = ec2.create_subnet(
+            Id=id,
+            CidrBlock=cidr_block,
+            VpcId=vpc_id)
         print(response)
     except ClientError as e:
         print(e)
